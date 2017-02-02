308931c9696d6229d52a89582123cb5d022b564f
Dirk Gadsden <dirk@esherido.com>
2017-02-02 10:13:39 -0800

    Add abstract for the type-system

---
+++ src/main/java/org/hummingbirdlang/types/type.java
@@ -0,0 +1,26 @@
+package org.hummingbirdlang.types;
+
+/**
+ * Root of all representations and derivations of types.
+ *
+ * There are three key types in the type-system:
+ *
+ * 1. Concrete types: The fundamental types provided by the language.
+ *    Contains both value (eg. Int32) and reference (eg. Object) types.
+ *
+ * 2. Composite types: Built-in and user-defined types which can both:
+ *    derive (subclass) concrete types and/or compose (tuples & generics)
+ *    other types.
+ *
+ * 3. Unknown types: This is how the language supports ad-hoc and parametric
+ *    polymorphism. The type-checker and/or run-time will eventually realize
+ *    these into concrete or composite types.
+ *
+ * The general idea is that any code initially begins as a mish-mash of all
+ * three kinds of types. The type-checker's inference algorithm then infers
+ * and constrains as many types as possible into concrete or composite types.
+ * Finally the run-time uses that existing type information in tandem with
+ * run-time type information to perform as-optimized-as-possible evaluation.
+ */
+public abstract class Type {
+}
