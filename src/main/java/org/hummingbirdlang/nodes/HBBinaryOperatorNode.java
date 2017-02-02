fbce6beb8d05fd398e7281b528c286fd370b9d73
Dirk Gadsden <dirk@esherido.com>
2017-02-01 18:50:04 -0800

    Start building out logical comparison nodes

--- src/main/java/org/hummingbirdlang/nodes/HBBinaryOperatorNode.java
+++ src/main/java/org/hummingbirdlang/nodes/HBBinaryOperatorNode.java
@@ -1,16 +1,12 @@
 package org.hummingbirdlang;
 
-import com.oracle.truffle.api.dsl.NodeChild;
-import com.oracle.truffle.api.dsl.NodeChildren;
-import com.oracle.truffle.api.dsl.NodeField;
-import com.oracle.truffle.api.frame.VirtualFrame;
-
 // Basic identifier of a local or global name.
-@NodeChildren({@NodeChild("leftNode"), @NodeChild("rightNode")})
-@NodeField(name = "operator", type = HBBinaryOperator.class)
-public class HBBinaryOperatorNode extends HBExpressionNode {
-  @Override
-  public Object executeGeneric(VirtualFrame frame) {
-    return null;
+public abstract class HBBinaryOperatorNode extends HBExpressionNode {
+  @Child protected HBExpressionNode leftNode;
+  @Child protected HBExpressionNode rightNode;
+
+  protected HBBinaryOperatorNode(HBExpressionNode leftNode, HBExpressionNode rightNode) {
+    this.leftNode = leftNode;
+    this.rightNode = rightNode;
   }
 }
---
+++ src/main/java/org/hummingbirdlang/nodes/HBLogicalAndNode.java
@@ -0,0 +1,19 @@
+package org.hummingbirdlang;
+
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
+
+@NodeInfo(shortName = "&&")
+public class HBLogicalAndNode extends HBBinaryOperatorNode {
+  public HBLogicalAndNode(HBExpressionNode leftNode, HBExpressionNode rightNode) {
+    super(leftNode, rightNode);
+  }
+
+  @Override
+  public Object executeGeneric(VirtualFrame frame) {
+    // TODO: Better truthiness!
+    Object leftValue = this.leftNode.executeGeneric(frame);
+    Object rightValue = this.rightNode.executeGeneric(frame);
+    return (leftValue != null) && (rightValue != null);
+  }
+}
