36c43be6f365107373e93138c189b17274916269
Dirk Gadsden <dirk@esherido.com>
2017-02-03 18:36:08 -0800

    Add name and constructor to class type

--- src/main/java/org/hummingbirdlang/types/ClassType.java
+++ src/main/java/org/hummingbirdlang/types/ClassType.java
@@ -1,5 +1,11 @@
 package org.hummingbirdlang.types;
 
 public class ClassType extends CompositeType {
+  private String name;
   private Type superClass;
+
+  public ClassType(String name, Type superClass) {
+    this.name = name;
+    this.superClass = superClass;
+  }
 }
