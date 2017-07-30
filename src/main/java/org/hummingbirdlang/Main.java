08311930594725b5674364dcf9149136cc45721a
Dirk Gadsden <dirk@esherido.com>
2017-07-30 01:01:47 -0700

    Add some logging to start-up

--- src/main/java/org/hummingbirdlang/Main.java
+++ src/main/java/org/hummingbirdlang/Main.java
@@ -2,16 +2,18 @@ package org.hummingbirdlang;
 
 import java.io.File;
 import java.io.IOException;
+import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.api.vm.PolyglotEngine;
 
 public final class Main {
     public static void main(String[] args) throws IOException, RuntimeException {
-      PolyglotEngine engine = PolyglotEngine.newBuilder().build();
+      System.out.println("(running on " + Truffle.getRuntime().getName() + ")");
+
+      PolyglotEngine engine = PolyglotEngine.newBuilder().setIn(System.in).setOut(System.out).build();
       assert engine.getLanguages().containsKey(HBLanguage.MIME_TYPE);
 
       Source source = Source.newBuilder(new File(args[0])).build();
-
       engine.eval(source);
     }
 }
