09f0c5372f61e8d53c175e8b40c0bbac704008f1
Dirk Gadsden <dirk@esherido.com>
2017-08-16 03:00:47 -0700

    Add public constructor and unit helper constructor for tuple type

--- src/main/java/org/hummingbirdlang/types/TupleType.java
+++ src/main/java/org/hummingbirdlang/types/TupleType.java
@@ -3,6 +3,14 @@ package org.hummingbirdlang.types;
 public class TupleType extends CompositeType {
   private Type[] elements;
 
+  public TupleType(Type[] elements) {
+    this.elements = elements;
+  }
+
+  public static TupleType unit() {
+    return new TupleType(new Type[0]);
+  }
+
   @Override
   public Property getProperty(String name) throws PropertyNotFoundException {
     throw new PropertyNotFoundException("Not yet implemented");
